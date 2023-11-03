101bf27e027a7045ff90a2d74d36053439cd1d23
asmitaupreti <asmitaupreti52@gmail.com>
2023-11-03 15:25:09 +0100

    Working in token and refresh token cookies

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-  baseURL: "http://192.168.128.114:5101/api/",
+  baseURL: "http://192.168.128.111:5101/api/",
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Content-Type": "application/json",
